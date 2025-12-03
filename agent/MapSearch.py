46a1fc493c326bf24ace1fc44840b1bb4e20220f
uye <99072975+ABA2396@users.noreply.github.com>
2025-12-03 16:03:46 +0800

    feat: 当前地图捡食材

---
+++ agent/MapSearch.py
@@ -0,0 +1,48 @@
+from maa.agent.agent_server import AgentServer
+from maa.custom_action import CustomAction
+from maa.context import Context
+
+@AgentServer.custom_action("MapSearch_RowReset")
+class MapSearchRowReset(CustomAction):
+    def run(
+        self,
+        context: Context,
+        argv: CustomAction.RunArg,
+    ) -> CustomAction.RunResult:
+        try:
+            context.clear_hit_count("MapSearch_MoveH_LR")
+            context.clear_hit_count("MapSearch_MoveH_RL")
+        except Exception:
+            return CustomAction.RunResult(success=False)
+
+        return CustomAction.RunResult(success=True)
+
+@AgentServer.custom_action("MapSearch_ClearLevelHits1")
+class MapSearchClearLevelHits(CustomAction):
+    def run(self, context: Context, argv: CustomAction.RunArg) -> CustomAction.RunResult:
+        try:
+            context.clear_hit_count("MapSearch_Recognize_Level1")
+        except Exception:
+            return CustomAction.RunResult(success=False)
+
+        return CustomAction.RunResult(success=True)
+    
+@AgentServer.custom_action("MapSearch_ClearLevelHits2")
+class MapSearchClearLevelHits2(CustomAction):
+    def run(self, context: Context, argv: CustomAction.RunArg) -> CustomAction.RunResult:
+        try:
+            context.clear_hit_count("MapSearch_Recognize_Level2")
+        except Exception:
+            return CustomAction.RunResult(success=False)
+
+        return CustomAction.RunResult(success=True)
+
+@AgentServer.custom_action("MapSearch_ClearLevelHits3")
+class MapSearchClearLevelHits3(CustomAction):
+    def run(self, context: Context, argv: CustomAction.RunArg) -> CustomAction.RunResult:
+        try:
+            context.clear_hit_count("MapSearch_Recognize_Level3")
+        except Exception:
+            return CustomAction.RunResult(success=False)
+
+        return CustomAction.RunResult(success=True)
--- agent/main.py
+++ agent/main.py
@@ -3,8 +3,7 @@ import sys
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
 
-import my_action
-import my_reco
+from MapSearch import *
 
 
 def main():
@@ -13,9 +12,9 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <socket_id>")
         print("socket_id is provided by AgentIdentifier.")
-        sys.exit(1)
-        
-    socket_id = sys.argv[-1]
+        socket_id = "114514"
+    else:
+        socket_id = sys.argv[-1]
 
     AgentServer.start_up(socket_id)
     AgentServer.join()
